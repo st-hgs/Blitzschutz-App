f963b639c4711da27e5fa1082eea8c549139f591
st-hgs <St-haus-garten-service@web.de>
2026-09-24 22:08:56 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Blitzschutz Aufmaß", layout="centered")
+
+st.title("⚡ Blitzschutz & Erdung - Messprotokoll")
+st.write("Willkommen in deiner Aufmaß-App!")
+
+st.success("App erfolgreich gestartet!")
